517752c184e1e6e4f56d5cfbd1b5607f623a14f6
rukenDogan <ruken.dogan.rkn@gmail.com>
2025-05-18 15:09:14 +0200

    modif pomme et serpent

--- js/controllerSnake.js
+++ js/controllerSnake.js
@@ -1,7 +1,12 @@
 let snake = document.getElementById("snake");
-let positionDeMonSnakeY = 40;
+let positionDeMonSnakeY = 50;
 let positionDeMonSnakeX = 50;
 
+let apple = document.getElementById("apple");
+apple.style.left = "100px";
+apple.style.top = "100px";
+
+let segments = [snake];
 
 
 function mouveSnakeDown() {
@@ -9,7 +14,6 @@ function mouveSnakeDown() {
     snake.style.top = positionDeMonSnakeY + "px";
     if (positionDeMonSnakeY == 480) {
     positionDeMonSnakeY = 0;
-    console.log(positionDeMonSnakeY);
     }
     
 }
@@ -18,8 +22,7 @@ function mouveSnakeUp() {
     positionDeMonSnakeY -= 10;
     snake.style.top = positionDeMonSnakeY + "px";
     if (positionDeMonSnakeY == 0) {
-    positionDeMonSnakeY = 580;
-    console.log(positionDeMonSnakeY);
+    positionDeMonSnakeY = 490;
     }
 }
 
@@ -47,8 +50,26 @@ let keydownSave = "";
 
 document.addEventListener('keydown', function (event) {
     keydownSave = event.key
-    setInterval(mouveSnake(keydownSave), 100);
 });
+
+setInterval(function () {
+
+    for (let i = segments.length - 1; i > 0; i--) {
+        segments[i].style.left = segments[i - 1].style.left;
+        segments[i].style.top = segments[i - 1].style.top;
+    }
+
+    mouveSnake(keydownSave);
+
+    if (
+    positionDeMonSnakeX === parseInt(apple.style.left) &&
+    positionDeMonSnakeY === parseInt(apple.style.top)
+    ) {
+    growSnake();
+    repositionApple();
+    }
+
+}, 190);
 
 
 function mouveSnake(keydown) {
@@ -71,3 +92,22 @@ function mouveSnake(keydown) {
 }
 
 
+function growSnake() {
+    let snake = document.getElementById("snake");
+    let newSegment = snake.cloneNode(true);
+    let lastSegment = segments[segments.length - 1];
+
+    newSegment.style.left = lastSegment.style.left;
+    newSegment.style.top = lastSegment.style.top;
+
+    document.querySelector('.game-area').appendChild(newSegment);
+    segments.push(newSegment);
+}
+
+function repositionApple() {
+    let randomX = Math.floor(Math.random() * 60) * 10;
+    let randomY = Math.floor(Math.random() * 50) * 10; 
+
+    apple.style.left = randomX + "px";
+    apple.style.top = randomY + "px";
+}
